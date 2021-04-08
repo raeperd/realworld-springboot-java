fb0233157a3aaba26872387db813549ca661fe8d
raeperd <raeperd117@gmail.com>
2021-04-08 22:36:07 +0900

    Initial commit

---
+++ .github/ISSUE_TEMPLATE/feature.md
@@ -0,0 +1,8 @@
+---
+name: Feature request
+about: Suggest an idea for this project
+title: '[FEATURE]'
+labels: 'feature'
+assignees: 'raeperd117@gmail.com'
+
+---
---
+++ src/main/java/io/github/raeperd/realworld/RealWorldApplication.java
@@ -0,0 +1,13 @@
+package io.github.raeperd.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldApplication.class, args);
+	}
+
+}
