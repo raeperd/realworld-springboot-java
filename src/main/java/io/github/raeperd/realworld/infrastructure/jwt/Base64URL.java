2a4cffde99e906428a53d366f4235a1f449c7f92
raeperd <raeperd117@gmail.com>
2021-05-15 03:47:21 +0900

    [#3] Implements Base64URL encode / decode

---
+++ src/main/java/io/github/raeperd/realworld/infrastructure/jwt/Base64URL.java
@@ -0,0 +1,20 @@
+package io.github.raeperd.realworld.infrastructure.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+class Base64URL {
+
+    private Base64URL() {
+    }
+
+    static String base64URLFromString(String rawString) {
+        return Base64.getUrlEncoder().withoutPadding()
+                .encodeToString(rawString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    static String stringFromBase64URL(String base64URL) {
+        return new String(Base64.getUrlDecoder().decode(base64URL));
+    }
+
+}
