e12836e7fe53696fde5d14d319892941daa36068
raeperd <raeperd117@gmail.com>
2021-05-25 19:45:05 +0900

    [#33] Implements slugFromTitle

--- src/main/java/io/github/raeperd/realworld/domain/article/ArticleTitle.java
+++ src/main/java/io/github/raeperd/realworld/domain/article/ArticleTitle.java
@@ -12,6 +12,26 @@ class ArticleTitle {
     @Column(nullable = false)
     private String slug;
 
+    public static ArticleTitle of(String title) {
+        return new ArticleTitle(title, slugFromTitle(title));
+    }
+
+    private ArticleTitle(String title, String slug) {
+        this.title = title;
+        this.slug = slug;
+    }
+
     protected ArticleTitle() {
+    }
+
+    private static String slugFromTitle(String title) {
+        return title.toLowerCase()
+                .replaceAll("\\$,'\"|\\s|\\.|\\?", "-")
+                .replaceAll("-{2,}", "-")
+                .replaceAll("(^-)|(-$)", "");
+    }
+
+    String getSlug() {
+        return slug;
     }
 }
