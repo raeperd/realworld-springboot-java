5d4d4680167eb6eb36ec52b0906a50191ad4b29f
raeperd <raeperd117@gmail.com>
2021-06-01 23:56:34 +0900

    [#33] Refactor ArticleService.createNewArticle - Tags needs to be reloaded to avoid database unique constraints

--- src/main/java/io/github/raeperd/realworld/domain/article/ArticleContents.java
+++ src/main/java/io/github/raeperd/realworld/domain/article/ArticleContents.java
@@ -48,4 +48,12 @@ public class ArticleContents {
     public String getBody() {
         return body;
     }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
 }
