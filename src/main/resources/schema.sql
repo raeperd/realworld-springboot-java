e8240f1419f3a7edca9ac6505962e6aa28106e21
martin <martinrpetersen171@gmail.com>
2021-08-02 18:26:54 -0300

    - Creates the new Report table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -66,3 +66,14 @@ CREATE TABLE IF NOT EXISTS comments
     CONSTRAINT fk_comment_author FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_comment_article FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS comment_reports
+(
+  id          BIGSERIAL PRIMARY KEY,
+  comment_id  BIGINT NOT NULL,
+  user_id     BIGINT NOT NULL,
+  article_title VARCHAR(255) NOT NULL,
+  body        VARCHAR NOT NULL,
+  CONSTRAINT fk_reported_comment FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE,
+  CONSTRAINT fk_report_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  );
