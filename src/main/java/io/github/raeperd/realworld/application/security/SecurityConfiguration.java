f8cac98682eeeb41a5b29d4a0ac5d6152f05a929
raeperd <raeperd117@gmail.com>
2021-09-25 03:45:33 +0900

    Add CORS configuration for frontend test

--- src/main/java/io/github/raeperd/realworld/application/security/SecurityConfiguration.java
+++ src/main/java/io/github/raeperd/realworld/application/security/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package io.github.raeperd.realworld.application.security;
 
 import io.github.raeperd.realworld.domain.jwt.JWTDeserializer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,16 +11,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
+@EnableConfigurationProperties(SecurityConfigurationProperties.class)
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    private final SecurityConfigurationProperties properties;
+
+    SecurityConfiguration(SecurityConfigurationProperties properties) {
+        this.properties = properties;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors();
+        http.formLogin().disable();
+        http.logout().disable();
         http.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.authorizeRequests()
                 .antMatchers(POST, "/users", "/users/login").permitAll()
@@ -36,4 +53,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("GET", "POST", "DELETE")
+                .allowedOrigins(properties.getAllowedOrigins().toArray(new String[0]))
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
+@ConstructorBinding
+@ConfigurationProperties("security")
+class SecurityConfigurationProperties {
+    private final List<String> allowedOrigins;
+
+    SecurityConfigurationProperties(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
 }
